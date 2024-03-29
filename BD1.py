7b7092d484cd54c7b1c5d8133fb18814d17341c3
vsudov <114310019+vsudov@users.noreply.github.com>
2024-03-29 12:26:57 +0300

    init

---
+++ BD1.py
@@ -0,0 +1,98 @@
+import sqlite3
+
+class Model:
+    def __init__(self):
+        self.connection = sqlite3.connect('database.db')
+        self.cursor = self.connection.cursor()
+    
+    def add_record(self, data):
+        query = 'INSERT INTO users (name, surname, yearsOld, email) VALUES (?, ?, ?, ?)'
+        self.cursor.execute(query, (data['name'], data['surname'], data['yearsOld'], data['email']))
+        self.connection.commit()
+        print('Данные добавлены.')
+    
+    def delete_record(self, id):
+        query = 'DELETE FROM users WHERE id = ?'
+        self.cursor.execute(query, (id,))
+        self.connection.commit()
+        print('Строка удалена.')
+        
+    def search_record(self, value):
+        query = f"SELECT * FROM users WHERE name = '{value}' OR surname = '{value}' OR email = '{value}'"
+        result = self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        return rows
+
+    def show_all_records(self):
+        query = 'SELECT * FROM users'
+        results = self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        
+        if(rows):
+            print('№:\tИмя:\tФамилия:\tВозраст:\tЭл. почта:')
+            for row in rows:
+                print(str(row[0]) + '\t' + str(row[1]) + '\t' + str(row[2]) + '\t' + str(row[3]) + '\t' + str(row[4]))
+        else:
+            print('Записей нет.')
+
+class Presenter:
+    model = Model()
+    
+    def add(self):
+        name = input('Введите имя:\t')
+        surname = input('Введите фамилию:\t')
+        yearsOld = int(input('Введите возраст:\t'))
+        email = input('Введите эл. почту:\t')
+        
+        data = {
+            'name': name,
+            'surname': surname,
+            'yearsOld': yearsOld,
+            'email': email
+        }
+        
+        self.model.add_record(data)
+        
+    def delete(self, record_id):
+        self.model.delete_record(record_id)
+
+    def search(self, value):
+        records = self.model.search_record(value)
+        if records:
+            print('Найдено совпадение:')
+            print('№:\tИмя:\tФамилия:\tВозраст:\tЭл. почта:')
+            for row in records:
+                print(str(row[0]) + '\t' + str(row[1]) + '\t' + str(row[2]) + '\t' + str(row[3]) + '\t' + str(row[4]))
+        else:
+            print('Совпадений не найдено')
+
+    def view_all(self):
+        self.model.show_all_records()
+
+presenter = Presenter()
+
+while True:
+    print('''1 - Добавить запись
+2 - Удалить запись
+3 - Найти запись
+4 - Показать все записи
+5 - Выход''')
+    comand = int(input('Выберите команду:\t'))
+    
+    if(comand == 5):
+        print('Выход выполнен.')
+        break
+    elif(comand == 1):
+        presenter.add()
+    elif(comand == 2):
+        presenter.view_all()
+        choice = int(input('Выберите строку для удаления:\t'))
+        presenter.delete(choice)
+    elif(comand == 3):
+        searchValue = input('Введите текст для поиска:\t')
+        presenter.search(searchValue)
+    elif(comand == 4):
+        presenter.view_all()
+    else:
+        print('Неизвестная команда!')
+    
